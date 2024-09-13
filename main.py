c3efdc333d662e356b1778f7342fac92863b56dd
Vladimir Sorokin <bcc20080913@yandex.ru>
2024-09-13 22:52:20 +1000

    0.0.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import difflib
 import requests
 import html
+import hashlib
 
 def file(fn):
     r = requests.get(fn)
@@ -10,16 +11,31 @@ from html.parser import HTMLParser
 class HTMLParser(HTMLParser):
     nn = 0
     ll = []
+    au = []
+    mc = hashlib.sha256()
+    cc = ('class', 'name', 'id')
+
     def handle_starttag(self, tag, attrs):
         if tag=='html':
             self.nn = 0
         elif tag == 'body':
             self.nn = 1
         else: self.nn += 1
-        self.ll.append(str(self.nn)+' '+tag)
+
+        mc = hashlib.md5()
+        mc.update(str(self.nn).encode())
+        mc.update(tag.encode())
+
+        for attr, value in attrs:
+            if attr in self.cc:
+                mc.update(value.encode())
+
+        self.ll.append([self.nn, tag, mc.hexdigest()])
         if tag=='head' or tag=='body' or tag=='html':
             print('bb',tag,self.nn)
             print(attrs)
+        nu = [attr[0] for attr in attrs if attr[0] not in self.au]
+        self.au.extend(nu)
 
     def handle_endtag(self, tag):
         #print("Encountered an end tag :", tag)
@@ -39,6 +55,7 @@ def refine (rn):
     parser.close()
     print(parser.nn)
     print(parser.ll)
+    print(len(parser.au),parser.au)
     return parser.ll
 
 def main():
