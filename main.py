9ed507080f3bd50a2ea4f66680a09fdc2f4e807f
Vladimir Sorokin <bcc20080913@yandex.ru>
2024-09-13 23:18:05 +1000

    0.0.2

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class HTMLParser(HTMLParser):
     au = []
     mc = hashlib.sha256()
     cc = ('class', 'name', 'id')
+    aappend = False
 
     def handle_starttag(self, tag, attrs):
         if tag=='html':
@@ -30,43 +31,59 @@ class HTMLParser(HTMLParser):
             if attr in self.cc:
                 mc.update(value.encode())
 
-        self.ll.append([self.nn, tag, mc.hexdigest()])
+        if tag == 'body':
+            self.aappend = True
+
+        if self.aappend:
+            self.ll.append([self.nn, tag, mc.hexdigest()])
+
+        '''
         if tag=='head' or tag=='body' or tag=='html':
             print('bb',tag,self.nn)
             print(attrs)
+        '''
+        '''    
         nu = [attr[0] for attr in attrs if attr[0] not in self.au]
         self.au.extend(nu)
+        '''
 
     def handle_endtag(self, tag):
-        #print("Encountered an end tag :", tag)
+        """
         if tag=='head' or tag=='body' or tag=='html':
             print('ee',tag,self.nn)
+        """
         self.nn -= 1
 
     #def handle_data(self, data):
     #    print("Encountered some data  :", data)
 
 def refine (rn):
-    ll = []
-    nn = 0
     rcontent = str(rn)
+
     parser = HTMLParser()
     parser.feed(rcontent)
     parser.close()
-    print(parser.nn)
-    print(parser.ll)
-    print(len(parser.au),parser.au)
+
+    #print(parser.nn)
+    #print(parser.ll)
+    #print(len(parser.au),parser.au)
     return parser.ll
 
 def main():
     print('привет')
     r1 = file('https://github.com/NelliSm/Sprint_5/pulls')
-    print(f'\n{len(r1.text)}')
-    nlist = refine(r1.content.replace(b'\n',b''))
-    print(len(nlist))
+    #print(f'\n{len(r1.text)}')
+    nlist1 = refine(r1.content.replace(b'\n',b''))
+    #print(len(nlist))
     #r1content = r1.content.replace(b'\n',b'')
     #print(r1content)
-    #print('Досвидос')
+    r2 = file('https://github.com/SorokinV/bobaDiff/pulls')
+    nlist2 = refine(r2.content.replace(b'\n',b''))
+
+    for i in range(30):
+        print(nlist1[i])
+        print(nlist2[i])
+    print('Досвидос')
     return()
 
 main()
